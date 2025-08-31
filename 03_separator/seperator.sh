4b9913c15a48db95024a92a487993d9fef9d17d0
Brajananda Mahata <brajanandamahato280603@gmail.com>
2025-09-01 00:20:13 +0530

    assignment 3 done

---
+++ 03_separator/seperator.sh
@@ -0,0 +1,31 @@
+echo -e "\n\nEnter a list of numbers separated by commas ',' : \n"
+
+read numStr
+
+IFS=',' 
+read -a numArray <<< "$numStr"
+
+
+odds=""
+evens=""
+
+
+for number in "${numArray[@]}"
+do
+	if [[ number%2 -eq 0 ]]; then
+		evens+="$number"
+		evens+=" "
+	else
+		odds+="$number"
+		odds+=" "
+	fi
+done
+
+
+touch odd.txt
+touch even.txt
+
+echo "$evens" > even.txt
+echo "$odds" > odd.txt
+
+echo -e "\n\nOdd numbers are saved in ./odd.txt \nEven numbers are saved in ./even.txt"
