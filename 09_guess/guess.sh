2b637754bd455a2e091b3045b2c3280bc923cc9a
Brajananda Mahata <brajanandamahato280603@gmail.com>
2025-09-09 01:27:22 +0530

    shows the number of guesses

--- 09_guess/guess.sh
+++ 09_guess/guess.sh
@@ -1,9 +1,10 @@
 random=$((1+ $RANDOM %50))
+count=0
 while true;
 do
 	echo -e "\nEnter Guess: "
 	read user
-
+	count=$((count + 1))
 	if (( random < user )); then
 		echo -e "\nToo High"
 	else 
@@ -11,6 +12,7 @@ do
 			echo -e "\nToo Low"
 		else
 			echo -e "\n\nCorrect The number was : $random"
+			echo -e "\nCorrectly guessed after $count times"
 			break
 		fi
 	fi
